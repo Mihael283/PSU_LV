f33d77590dd25261b1a721602b56c051dc088f80
Mihael Španović <span.mihael@gmail.com>
2021-03-22 00:25:44 +0100

    Chnages

--- LV2/Zadatak3.py
+++ LV2/Zadatak3.py
@@ -2,25 +2,22 @@ import numpy as np
 from matplotlib import pyplot
 from matplotlib import pyplot as plt
 
-data = np.loadtxt(open("/home/profesor/PSU_LV/LV2/mtcars.csv","rb"), usecols=(1,2,3,4,5,6), delimiter=",", skiprows=1)
+data = np.loadtxt(open("LV2\mtcars.csv","rb"), usecols=(1,2,3,4,5,6), delimiter=",", skiprows=1)
 print(data)
 
-
-pyplot.scatter(data[:,0],data[:,3],c='b',s=data[:,2])
-
+pyplot.scatter(data[:,0],data[:,3],c='b',s=data[:,5]*20)
 
 print(min(data[:,0]))
 print(max(data[:,0]))
-print(sum(data[:,0]))
+print(sum(data[:,0])/len(data[:,0]))
 arr=[]
-for i in data[:,1]:
-    if i>=6:
-        arr.append(i)
 
+for i,item in enumerate(data[:,1]):
+    if item >=6:
+        arr.append(data[i,0])
+        
 print(min(arr))
 print(max(arr))
-print(sum(arr))
+print(sum(arr)/len(arr))
     
-
-
 plt.show()
